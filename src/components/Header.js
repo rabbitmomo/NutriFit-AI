26df655bcfbcded5a124ae99eefd8e450055b440
cole71 <niche0913@gmail.com>
2025-01-01 18:58:49 +0800

    update logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,19 @@ import "../css/header.css";
 
 const Header = () => {
   return (
-    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: "#ff9e80", }}>
+    <AppBar
+      position="fixed"
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        backgroundColor: "#ff9e80",
+      }}
+    >
       <Toolbar>
+        <img
+          src="/logo.png" 
+          alt="NutriFit AI Logo"
+          style={{ height: "40px", marginRight: "10px" }}
+        />
         <Typography variant="h5" component="div">
           NutriFit AI
         </Typography>
