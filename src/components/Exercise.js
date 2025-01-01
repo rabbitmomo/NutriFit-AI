617181dc45998d2063f051b406ea79d502899ed2
janetteg1 <janettegann17@gmail.com>
2025-01-01 17:49:02 +0800

    update

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -49,7 +49,7 @@ const Exercise = () => {
   return (
     <Box sx={{ padding: 3 }}>
       <Typography variant="h4" gutterBottom align="center">
-        Latest Exercisesesesesesesllllllllll
+        Latest Exercisesesesesesesllllllllldfydyfhfhfhfhl
       </Typography>
 
       <Grid container spacing={3}>
